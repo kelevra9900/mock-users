ad0ce824c3a38643f8170acb4e08d30ec1776b25
Roger Torres <torresroger445@gmail.com>
2023-09-02 14:46:42 -0600

    feat: :art: loader component

---
+++ src/components/ui/loader.tsx
@@ -0,0 +1,32 @@
+import {View,Text,ActivityIndicator,StyleSheet} from "react-native";
+// import LottieView from "lottie-react-native";
+
+
+type LoaderType = {
+	text?: string
+}
+
+const LoaderComponent = ({text = 'Loading...'}: LoaderType) => {
+	return (
+		<View style={styles.container}>
+			<ActivityIndicator size="large" color="#007AFF" />
+			<Text style={styles.text}>{text}</Text>
+		</View>
+	);
+}
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	text: {
+		marginTop: 10,
+		fontSize: 16,
+		color: '#333',
+	},
+})
+
+export default LoaderComponent;
