e284abcccafa81b4ef214f0ba0df88a41baca859
Roger Torres <torresroger445@gmail.com>
2023-09-02 19:09:15 -0600

    style: :art: style: :package: safeareaprovider and configuration

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {QueryClient,QueryClientProvider} from '@tanstack/react-query';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
+import HomeScreen from '@screens/Home'
+import SplashScreen from '@screens/Splash'
+import UserDetail from '@screens/UserDetail'
 
-import HomeScreen from '@screens/Home';
-import UserDetail from '@screens/UserDetail';
-import {RootStackParamList} from './src/types/routes';
-import SplashScreen from '@screens/Splash';
+import { RootStackParamList } from './src/types/routes'
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 const queryClient = new QueryClient()
 
@@ -16,22 +17,14 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Splash"
-            component={SplashScreen}
-          />
-          <Stack.Screen
-            name="Home"
-            options={{headerShown: false}}
-            component={HomeScreen}
-          />
-          <Stack.Screen
-            name="Profile"
-            component={UserDetail}
-          />
-        </Stack.Navigator>
+        <SafeAreaProvider>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Splash" component={SplashScreen} />
+            <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
+            <Stack.Screen name="Profile" component={UserDetail} />
+          </Stack.Navigator>
+        </SafeAreaProvider>
       </NavigationContainer>
     </QueryClientProvider>
-  );
+  )
 }
